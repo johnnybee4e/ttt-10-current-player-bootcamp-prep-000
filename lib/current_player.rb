c1a53873de576978ac1130c98516800cf77394df
Jonathan Rosado <jon.rosado42@gmail.com>
2017-09-28 12:45:05 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,9 +9,11 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) % 2
-    puts "X"
-  else
-    puts"O"
+  board.each do |token|
+    if turn_count(board) % 2
+      puts "X"
+    else
+      puts"O"
+    end
   end
 end
